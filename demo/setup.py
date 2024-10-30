03a511fb897beba2edec5b474092584af97e8dc8
CoreyAllan_Rynders <car2r@virginia.edu>
2024-10-29 20:20:27 -0400

    Initial commit: Add the Demo package

---
+++ demo/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Demo",
+    version="0.1",
+    author="CAR",
+    author_email="email@example.com",
+    description="Python package with one function",
+    packages=find_packages(),
+    license='MIT',
+    python_requires=">=3.6",
+)
